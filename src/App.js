9e1e02d84f44737770fedfb8657c8d6773f85b63
SantiagoJSuarez <58789506+SantiagoJSuarez@users.noreply.github.com>
2022-01-27 20:59:18 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenido a Juiceture !!!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Conoce Mas Aqui !!!
         </a>
       </header>
     </div>
